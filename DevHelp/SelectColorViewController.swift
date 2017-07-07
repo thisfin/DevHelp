6938942e98984fdf72dc4a61d783aa5cfa115cba
文优 <wenyou.ly@taobao.com>
2017-07-07 17:43:16 +0800

    update

--- DevHelp/SelectColorViewController.swift
+++ DevHelp/SelectColorViewController.swift
@@ -11,8 +11,8 @@ import AppKit
 class SelectColorViewController: NSViewController {
     let frontView = ColorView()
     let backView = ColorView()
-
     let textField = ColorTextField()
+    var nowColorView: ColorView?
 
     override func loadView() {
         view = NSView()
@@ -47,6 +47,8 @@ class SelectColorViewController: NSViewController {
         frontView.wantsLayer = true
         backView.layer?.backgroundColor = NSColor.green.cgColor
         frontView.layer?.backgroundColor = NSColor.blue.cgColor
+        backView.contorller = self
+        frontView.contorller = self
 
         let label = NSTextField(labelWithString: "RGB: 6/8位 16进制")
         view.addSubview(label)
@@ -64,22 +66,25 @@ class SelectColorViewController: NSViewController {
         textField.delegate = self
 
 
-//        let frontTextField = NSTextField()
-//        let backTextField = NSTextField()
-
         textField.callbackBlock = { (color) in
-            self.frontView.layer?.backgroundColor = color.cgColor
+            if let view = self.nowColorView {
+                view.setNowColor(color: color)
+            }
         }
         frontView.callbackBlock = { (color) in
-            self.textField.stringValue = (color.toHexString() as NSString).substring(from: 2)
+            self.textField.stringValue = color.toHexString(prefix: .null, includeAlpha: true)
         }
         backView.callbackBlock = { (color) in
-            self.textField.stringValue = (color.toHexString() as NSString).substring(from: 2)
+            self.textField.stringValue = color.toHexString(prefix: .null, includeAlpha: true)
         }
     }
 
     override func viewWillAppear() {
         super.viewWillAppear()
+    }
+
+    func textColorChange(color: NSColor) {
+
     }
 }
 
@@ -95,16 +100,17 @@ class ColorTextField: NSTextField {
     override func textDidChange(_ notification: Notification) {
         if let view = notification.object as? NSTextView, let str = view.string {
             let color = NSColor.colorWithString(str)
-            if let block = callbackBlock {
+            if let block = callbackBlock, color != .clear {
                 block(color)
             }
-            NSLog("\(str)")
         }
     }
 }
 
 class ColorView: NSView {
     var callbackBlock: ((NSColor) -> Void)?
+    weak var panel: NSColorPanel?
+    weak var contorller: SelectColorViewController?
 
     override func mouseDown(with event: NSEvent) {
         let panel = NSColorPanel.shared().then { (this) in
@@ -116,6 +122,21 @@ class ColorView: NSView {
             }
         }
         panel.makeKeyAndOrderFront(self)
+        if let cv = contorller {
+            cv.nowColorView = self
+        }
+        self.panel = panel
+    }
+
+    func setNowColor(color: NSColor) {
+        if let p = panel { // 设置颜色时关闭回调
+            let temp = callbackBlock
+            callbackBlock = nil
+            p.color = color
+            callbackBlock = temp
+        } else {
+            layer?.backgroundColor = color.cgColor
+        }
     }
 
     func colorSelect(_ sender: NSColorPanel) {
